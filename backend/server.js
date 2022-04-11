300d0c516b546033a23a5ebae351ff46f07cd3fb
Chris Lovejoy <christopher.lovejoy1@gmail.com>
2022-04-11 16:37:17 +0100

    added functional button for uploading files to s3

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,32 @@
-import express from 'express'
+const express = require('express')
+
+const multer = require('multer')
+
+const fileStorageEngine = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "files");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({storage: fileStorageEngine});
+
+const { uploadFile } = require('./s3')
 
 const app = express()
 
-app.use(express.static('front'))
+app.post('/api/images', upload.single('file'), async (req, res) => {
+  const file = req.file
+  console.log("File details (locally):\n", file)
 
+  const result = await uploadFile(file)
+  console.log("File details (on AWS S3):\n", result)
 
+  const description = req.body.description
 
+  res.send("🤗")
+})
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(8080, () => console.log("listening on port 8080"))
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,39 @@
+import { useState } from 'react'
+import axios from "axios"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const [file, setFile] = useState()
+  const [description, setDescription] = useState("")
+
+  const submit = async event => {
+    event.preventDefault()
+
+    const formData = new FormData()
+    formData.append("file", file)
+    formData.append("description", description)
+
+    const result = await axios.post('/api/images', formData, { headers: {'Content-Type': 'multipart/form-data'}})
+    console.log(result.data)
 }
 
-export default App;
+  return (
+    <div className="App">
+      <form onSubmit={submit}>
+        Upload file here (only accepts .txt files): <input
+          filename={file} 
+          onChange={e => setFile(e.target.files[0])} 
+          type="file" 
+          accept=".txt"
+        ></input>
+        <br></br>
+        Your name (to later be replaced with wallet address): 
+        <input
+          onChange={e => setDescription(e.target.value)} 
+          type="text"
+        ></input>
+        <br></br>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+    )
+  }
