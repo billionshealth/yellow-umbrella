dd0732b68e596bfb7b48cc94e603a8b915c801fc
Chris Lovejoy <christopher.lovejoy1@gmail.com>
2022-05-03 12:52:53 +0100

    add wallet linking functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,70 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { ethers } from 'ethers'
 import axios from "axios"
 import './App.css'
+
 
 export default function App() {
   const [file, setFile] = useState()
   const [description, setDescription] = useState("")
+  const [currentAccount, setCurrentAccount] = useState("");
 
-  const submit = async event => {
-    event.preventDefault()
+    const checkIfWalletIsConnected = async () => {
+        try {
+            const { ethereum } = window;
+            if (!ethereum) {
+                console.log("Make sure you have metamask!");
+                return;
+            } else {
+                console.log("We have the ethereum object", ethereum);
+            }
 
-    const formData = new FormData()
-    formData.append("file", file)
-    formData.append("description", description)
+            const accounts = await ethereum.request({ method: "eth_accounts" });
 
-    const result = await axios.post('/api/images', formData, { headers: {'Content-Type': 'multipart/form-data'}})
-    console.log(result.data)
-}
+            if (accounts.length !== 0) {
+                const account = accounts[0];
+                console.log("Found an authorized account:", account);
+                setCurrentAccount(account)
+            } else {
+                console.log("No authorized account found")
+            }
+            } catch (error) {
+            console.log(error);
+            }
+        }
+
+    const connectWallet = async () => {
+        try {
+            const { ethereum } = window;
+    
+            if (!ethereum) {
+            alert("Do you have MetaMask installed? If not, get it here: https://metamask.io/");
+            return;
+            }
+    
+            const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+    
+            console.log("Connected", accounts[0]);
+            setCurrentAccount(accounts[0]);
+        } catch (error) {
+            console.log(error)
+            }
+        }
+
+    useEffect(() => {
+    checkIfWalletIsConnected();
+    }, [])
+
+    const submit = async event => {
+        event.preventDefault()
+
+        const formData = new FormData()
+        formData.append("file", file)
+        formData.append("description", description)
+
+        const result = await axios.post('/api/images', formData, { headers: {'Content-Type': 'multipart/form-data'}})
+        console.log(result.data)
+    }
 
   return (
       <div className="mainContainer">
@@ -23,6 +72,17 @@ export default function App() {
             <div className="header">
                 Yellow Umbrella ☂ 
             </div>
+
+            {!currentAccount && (
+                <>
+                <div className='text-block'>
+                    Connect your wallet to get started!
+                </div>
+                <button className="walletButton" onClick={connectWallet}>
+                    Click here to connect wallet
+                </button>
+                </>
+            )}
 
             <div className="text-block">
                 Upload your DNA sequence to get a unique bio NFT
