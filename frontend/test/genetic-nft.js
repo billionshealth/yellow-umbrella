2218e2b0196694d6054597df2ca3603ac54bd1eb
Chris Lovejoy <christopher.lovejoy1@gmail.com>
2022-05-31 15:09:38 +0100

    add testing for initial functionality

---
+++ frontend/test/genetic-nft.js
@@ -0,0 +1,25 @@
+const { ethers } = require('hardhat');
+const { use, expect } = require('chai');
+const { solidity, MockProvider } = require('ethereum-waffle');
+
+describe("GeneticNFT", function() {
+    let geneticNFTFactory;
+
+    it("Should deploy geneticNFTfactory", async function () {
+     const geneticNFTContract = await ethers.getContractFactory('GeneticNFTFactory');
+     geneticNFTFactory = await geneticNFTContract.deploy();
+    });
+
+    it("Should create two tokens", async function() {
+      await geneticNFTFactory.createNFT("https://www.mytokenlocation.com")
+      await geneticNFTFactory.createNFT("https://www.mytokenlocation2.com")
+
+    });
+    
+    it("Should return the tokens", async function() {
+      const items = await geneticNFTFactory.fetchMyNFTs();
+      // TODO: decompose items and check the different variable values
+      console.log('items:', items)
+    });
+  
+  })
