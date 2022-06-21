6220cb9b7d8706d0b0b0a491307710fcb786fd04
MAbbasRaza <96629590+MAbbasRaza@users.noreply.github.com>
2022-06-21 13:57:53 +0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,13 +7,13 @@ const Navbar = () => {
         Yellow Umbrella ☂
       </div>
       <div className="links flex  justify-center m-auto md:w-full sm:w-2/12">
-        <button className=" rounded-full m-5 p-3 px-6 sm:p-2  xl:m-5 sm:m-1 sm:text-sm md:text-base lg:text-lg text-lightGray bg-midGray baseline hover:bg-lime-100 hover:duration-300 ">
+        <button className=" rounded-full m-5 p-3 px-6 sm:p-2  xl:m-5 sm:m-1 sm:text-sm md:text-base lg:text-lg baseline hover:bg-lime-1000 hover:duration-300 ">
           <Link to="/">Ancestry</Link>
         </button>
-        <button className=" rounded-full m-5 p-3 px-6 sm:p-2  xl:m-5 sm:m-1 sm:text-sm md:text-base lg:text-lg text-lightGray bg-midGray baseline hover:bg-lime-100 hover:duration-300">
+        <button className=" rounded-full m-5 p-3 px-6 sm:p-2  xl:m-5 sm:m-1 sm:text-sm md:text-base lg:text-lg baseline hover:bg-gray-1000 hover:duration-300">
           <Link to="/23andme">23andMe</Link>
         </button>
-        <button className=" rounded-full m-5 p-3 px-6 sm:p-2  xl:m-5 sm:m-1 sm:text-sm md:text-base lg:text-lg text-lightGray bg-midGray baseline hover:bg-lime-100 hover:duration-300 ">
+        <button className=" rounded-full m-5 p-3 px-6 sm:p-2  xl:m-5 sm:m-1 sm:text-sm md:text-base lg:text-lg baseline hover:bg-pink-1000 hover:duration-300 ">
           <Link to="/myheritage">MyHeritage</Link>
         </button>
       </div>
