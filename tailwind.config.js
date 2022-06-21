48791de73f93ce041988605ce5f8f64b2af99560
MAbbasRaza <96629590+MAbbasRaza@users.noreply.github.com>
2022-06-21 12:31:54 +0500

    Added custom screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      sm: { max: "767px" },
+      md: { min: "768px" },
+      lg: { min: "1024px" },
+      xl: { min: "1159px" },
+    },
     extend: {},
   },
+
   plugins: [],
 };
