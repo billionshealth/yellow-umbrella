e2af2b234ea04f2189961b94a23798870bb93039
MAbbasRaza <96629590+MAbbasRaza@users.noreply.github.com>
2022-06-21 13:31:40 +0500

    Update ancestry.js

--- src/pages/ancestry.js
+++ src/pages/ancestry.js
@@ -1,6 +1,6 @@
 const Ancestry = () => {
   return (
-    <div className="flex flex-col justify-center sm:p-0 sm:justify-start bg-lime-100 px-24 md:w-full xl:text-xl">
+    <div className="flex flex-col justify-center sm:p-0 sm:justify-start bg-lime-100 md:w-full xl:text-lg px-24 xl:px-64 ">
       <section className="top my-10 ">
         <h1 className="text-4xl xl:text-5xl sm:text-2xl font-extrabold text-gray-800 m-10 px-2 sm:m-0">
           How to download data from Ancestry
@@ -15,7 +15,7 @@ const Ancestry = () => {
         <div className="flex flex-col justify-center m-auto">
           <div className="m-5">
             <div className="flex 2xl:ml-36  xl:ml-20 ">
-              <p className="inline px-3 text-sky-700">1.</p>
+              <p className="inline px-3 text-sky-700">1</p>
               <p className="inline">Sign in to Ancestry.</p>
             </div>
             <img
@@ -26,7 +26,7 @@ const Ancestry = () => {
           </div>
           <div className="m-5">
             <div className="flex 2xl:ml-36  xl:ml-20">
-              <p className="inline px-3 text-sky-700">2.</p>
+              <p className="inline px-3 text-sky-700">2</p>
               <p className="inline">
                 Visit the{" "}
                 <a href="https://www.ancestry.com/account/signin">
@@ -43,7 +43,7 @@ const Ancestry = () => {
           </div>
           <div className="m-5">
             <div className="flex 2xl:ml-36  xl:ml-20">
-              <p className="inline px-3 text-sky-700">3.</p>
+              <p className="inline px-3 text-sky-700">3</p>
               <p className="inline">
                 Scroll down to the bottom “Actions” and enter your password,
                 check the box, and click ”Confirm”.
@@ -57,7 +57,7 @@ const Ancestry = () => {
           </div>
           <div className="m-5 ">
             <div className="flex 2xl:ml-36  xl:ml-20">
-              <p className="pl-3 text-sky-700">4.</p>
+              <p className="pl-3 text-sky-700">4</p>
               <p className="">
                 You'll get an email at the email address connected to your
                 Ancestry account. It contains a link to download your DNA file
@@ -68,7 +68,7 @@ const Ancestry = () => {
           </div>
           <div className="m-5">
             <div className="flex 2xl:ml-36  xl:ml-20">
-              <p className="inline px-3 text-sky-700">5.</p>
+              <p className="inline px-3 text-sky-700">5</p>
               <p className="inline">Upload your file here.</p>
             </div>
             <img
@@ -77,16 +77,16 @@ const Ancestry = () => {
               alt="Upload"
             />
           </div>
-          <div className="flex flex-col 2xl:w-4/12 mx-auto xl:w-4/12">
+          <div className="flex flex-col mx-auto xl:w-5/12">
             <button
               href="#"
-              className=" rounded-full m-5 p-3 sm:text-xs sm:p-2 md:p-2 md:text-md xl:text-lg xl:p-3 px-6 text-white bg-orange-400 baseline transition delay-150 ease-in-out hover:-translate-y-1 hover:scale-110 duration-300  border-2"
+              className=" rounded-full m-5 sm:text-xs sm:p-2 md:p-2 md:text-md  xl:p-3 px-6 text-white bg-orange-400 baseline transition delay-150 ease-in-out hover:-translate-y-1 hover:scale-110 duration-300  border-2"
             >
               GOT IT READY TO UPLOAD
             </button>
             <button
               href="https://www.ancestry.com/account/signin"
-              className=" rounded-full m-5 p-3 sm:text-xs sm:p-2 md:p-2 md:text-md xl:text-lg xl:p-3 px-6 text-lightGray bg-midGray baseline hover:bg-gray-100 hover:duration-300 border-2 border-gray-400"
+              className="rounded-full m-5 sm:text-xs sm:p-2 md:p-2 md:text-md xl:p-2 px-6 text-gray-800 baseline hover:bg-gray-300 hover:duration-300 duration-300 border-2 border-gray-400"
             >
               GO TO ANCESTRY AND DOWNLOAD
             </button>
