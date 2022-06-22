8805b97a6329e6d10443906d4a8cc92b17af3428
MAbbasRaza <96629590+MAbbasRaza@users.noreply.github.com>
2022-06-22 20:01:21 +0500

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './_app';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar2";
+import Ancestry from "./pages/ancestry";
+import TwentyThreeAndMe from "./pages/twentyThreeAndMe";
+import MyHeritage from "./pages/myheritage";
+import NotFound from "./pages/NotFound";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/ancestry" element={<Ancestry />} />
+        <Route path="/23andme" element={<TwentyThreeAndMe />} />
+        <Route path="/myheritage" element={<MyHeritage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
-  , document.getElementById('root')
 );
