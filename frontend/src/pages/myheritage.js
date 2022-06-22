c5da03ee69a214f5ab542c251d39bdceaa4efb16
MAbbasRaza <96629590+MAbbasRaza@users.noreply.github.com>
2022-06-22 20:27:59 +0500

    Update myheritage.js

--- frontend/src/pages/myheritage.js
+++ frontend/src/pages/myheritage.js
@@ -1,8 +1,8 @@
 const MyHeritage = () => {
   return (
-    <div className="flex flex-col justify-center sm:p-0 sm:justify-start bg-pink-1000 md:w-full xl:text-lg px-24 xl:px-64">
+    <div className="flex flex-col justify-center csm:p-0 csm:justify-start bg-pink-1000 cmd:w-full cxl:text-lg px-24 cxl:px-64">
       <section className="top my-10 ">
-        <h1 className="text-4xl xl:text-5xl sm:text-2xl font-extrabold text-gray-800 m-10 px-2 sm:m-0">
+        <h1 className="text-4xl cxl:text-5xl csm:text-2xl font-extrabold text-gray-800 m-10 px-2 csm:m-0">
           How to download data from MyHeritage
         </h1>
         <p className="px-10 w-4/5 my-2 m-auto">
@@ -14,7 +14,7 @@ const MyHeritage = () => {
       <section className="mid flex flex-col justify-center px-100">
         <div className="flex flex-col justify-center m-auto">
           <div className="m-5">
-            <div className="flex 2xl:ml-36  xl:ml-20 ">
+            <div className="flex 2xl:ml-36  cxl:ml-20 ">
               <p className="inline px-3 text-sky-700">1</p>
               <p className="inline">
                 Login to your{" "}
@@ -25,13 +25,13 @@ const MyHeritage = () => {
               </p>
             </div>
             <img
-              className="mx-auto my-5 md:w-11/12 sm:w-full lg:w-7/12"
+              className="mx-auto my-5 cmd:w-11/12 csm:w-full clg:w-7/12"
               src={process.env.PUBLIC_URL + "images/myheritage/step1.png"}
               alt="Sign In"
             />
           </div>
           <div className="m-5">
-            <div className="flex 2xl:ml-36  xl:ml-20">
+            <div className="flex 2xl:ml-36  cxl:ml-20">
               <p className="inline px-3 text-sky-700">2</p>
               <p className="inline">
                 Hover over the <b>DNA</b> tab and go to "<b>Manage DNA kits</b>
@@ -40,13 +40,13 @@ const MyHeritage = () => {
               </p>
             </div>
             <img
-              className="mx-auto my-5 md:w-11/12 sm:w-full lg:w-7/12"
+              className="mx-auto my-5 cmd:w-11/12 csm:w-full clg:w-7/12"
               src={process.env.PUBLIC_URL + "images/myheritage/step2.png"}
               alt="Raw Data"
             />
           </div>
           <div className="m-5">
-            <div className="flex 2xl:ml-36  xl:ml-20">
+            <div className="flex 2xl:ml-36  cxl:ml-20">
               <p className="inline px-3 text-sky-700">3</p>
               <p className="inline">
                 A window will pop up with information on what you are about to
@@ -57,13 +57,13 @@ const MyHeritage = () => {
               </p>
             </div>
             <img
-              className="mx-auto my-5 md:w-11/12 sm:w-full lg:w-7/12"
+              className="mx-auto my-5 cmd:w-11/12 csm:w-full clg:w-7/12"
               src={process.env.PUBLIC_URL + "images/myheritage/step3.png"}
               alt="Submit Raw Data Request"
             />
           </div>
           <div className="m-5 content-start">
-            <div className="flex 2xl:ml-36  xl:ml-20 ">
+            <div className="flex 2xl:ml-36  cxl:ml-20 ">
               <p className="inline px-3 text-sky-700">4</p>
               <p className="inline ">
                 An email with instructions will be sent to your registered email
@@ -73,7 +73,7 @@ const MyHeritage = () => {
             </div>
           </div>
           <div className="m-5 content-start">
-            <div className="flex 2xl:ml-36  xl:ml-20 ">
+            <div className="flex 2xl:ml-36  cxl:ml-20 ">
               <p className="inline px-3 text-sky-700">5</p>
               <p className="inline ">
                 Once you have clicked on the download link, you will be
@@ -84,26 +84,26 @@ const MyHeritage = () => {
             </div>
           </div>
           <div className="m-5">
-            <div className="flex 2xl:ml-36  xl:ml-20">
+            <div className="flex 2xl:ml-36  cxl:ml-20">
               <p className="inline px-3 text-sky-700">6</p>
               <p className="inline">Upload your file here.</p>
             </div>
             <img
-              className="mx-auto my-5 md:w-11/12 sm:w-full lg:w-7/12"
+              className="mx-auto my-5 cmd:w-11/12 csm:w-full clg:w-7/12"
               src={process.env.PUBLIC_URL + "images/myheritage/step6.png"}
               alt="Upload"
             />
           </div>
-          <div className="flex flex-col  mx-auto xl:w-5/12">
+          <div className="flex flex-col  mx-auto cxl:w-5/12">
             <button
               href="#"
-              className=" rounded-full m-5 sm:text-xs sm:p-2 md:p-2 md:text-md xl:text-lg xl:p-3 px-6 text-white bg-orange-400 baseline transition delay-150 ease-in-out hover:-translate-y-1 hover:scale-110 duration-300  border-2"
+              className=" rounded-full m-5 csm:text-xs csm:p-2 cmd:p-2 cmd:text-md cxl:text-lg cxl:p-3 px-6 text-white bg-orange-400 baseline transition delay-150 ease-in-out hover:-translate-y-1 hover:scale-110 duration-300  border-2"
             >
               GOT IT READY TO UPLOAD
             </button>
             <button
               href="https://www.myheritage.com"
-              className="rounded-full m-5 sm:text-xs sm:p-2 md:p-2 md:text-md xl:p-2 px-6 text-gray-800 baseline hover:bg-gray-300 hover:duration-300 duration-300 border-2 border-gray-400"
+              className="rounded-full m-5 csm:text-xs csm:p-2 cmd:p-2 cmd:text-md cxl:text-lg cxl:p-3 px-6 text-gray-800 baseline hover:bg-gray-300 hover:duration-300 duration-300 border-2 border-gray-400"
             >
               GO TO MyHeritage AND DOWNLOAD
             </button>
