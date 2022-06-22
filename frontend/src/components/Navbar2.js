7ef58f50aaeafa86ec1507720822368c83ce77f5
MAbbasRaza <96629590+MAbbasRaza@users.noreply.github.com>
2022-06-22 21:15:59 +0500

    Update Navbar2.js

--- frontend/src/components/Navbar2.js
+++ frontend/src/components/Navbar2.js
@@ -7,18 +7,26 @@ const Navbar = () => {
         Yellow Umbrella ☂
       </div>
       <div className="links flex  justify-center m-auto cmd:w-full csm:w-2/12">
-        <button className=" rounded-full m-5 p-3 px-6 csm:p-2  cxl:m-5 csm:m-1 csm:text-sm cmd:text-base clg:text-lg baseline hover:bg-yellow-100 hover:duration-300 ">
-          <Link to="/">Home</Link>
-        </button>
-        <button className=" rounded-full m-5 p-3 px-6 csm:p-2  cxl:m-5 csm:m-1 csm:text-sm cmd:text-base clg:text-lg baseline hover:bg-lime-1000 hover:duration-300 ">
-          <Link to="/ancestry">Ancestry</Link>
-        </button>
-        <button className=" rounded-full m-5 p-3 px-6 csm:p-2  cxl:m-5 csm:m-1 csm:text-sm cmd:text-base clg:text-lg baseline hover:bg-gray-1000 hover:duration-300">
-          <Link to="/23andme">23andMe</Link>
-        </button>
-        <button className=" rounded-full m-5 p-3 px-6 csm:p-2  cxl:m-5 csm:m-1 csm:text-sm cmd:text-base clg:text-lg baseline hover:bg-pink-1000 hover:duration-300 ">
-          <Link to="/myheritage">MyHeritage</Link>
-        </button>
+        <Link to="/">
+          <button className=" rounded-full m-5 p-3 px-6 csm:p-2  cxl:m-5 csm:m-1 csm:text-sm cmd:text-base clg:text-lg baseline hover:bg-yellow-100 hover:duration-300 duration-300">
+            Home
+          </button>
+        </Link>
+        <Link to="/ancestry">
+          <button className=" rounded-full m-5 p-3 px-6 csm:p-2  cxl:m-5 csm:m-1 csm:text-sm cmd:text-base clg:text-lg baseline hover:bg-lime-1000 hover:duration-300 duration-300">
+            Ancestry
+          </button>
+        </Link>
+        <Link to="/23andme">
+          <button className=" rounded-full m-5 p-3 px-6 csm:p-2  cxl:m-5 csm:m-1 csm:text-sm cmd:text-base clg:text-lg baseline hover:bg-gray-1000 hover:duration-300 duration-300">
+            23andMe
+          </button>
+        </Link>
+        <Link to="/myheritage">
+          <button className=" rounded-full m-5 p-3 px-6 csm:p-2  cxl:m-5 csm:m-1 csm:text-sm cmd:text-base clg:text-lg baseline hover:bg-pink-1000 hover:duration-300 duration-300">
+            MyHeritage
+          </button>
+        </Link>
       </div>
     </nav>
   );
