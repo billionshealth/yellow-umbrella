1d91ae023615975557735bdcbc1cb8a13c85f8c1
MAbbasRaza <96629590+MAbbasRaza@users.noreply.github.com>
2022-06-22 21:05:52 +0500

    Update NotFound.js

--- frontend/src/pages/NotFound.js
+++ frontend/src/pages/NotFound.js
@@ -2,15 +2,11 @@ import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    // <div className="not-found">
-    //   <h2>Sorry</h2>
-    //   <p>That page cannot be found</p>
-    //   <Link to="/">Back to the homepage...</Link>
-    // </div>
+    
     <div className="flex flex-col justify-center csm:p-0 csm:justify-start bg-pink-1000 cmd:w-full cxl:text-lg px-24 cxl:px-64">
       <section className="top my-10 ">
         <h1 className="text-4xl cxl:text-5xl csm:text-2xl font-extrabold text-gray-800 m-10 px-2 csm:m-0">
-          Sorry, That page cannot be found...
+          Sorry, Page Cannot Be Found...
         </h1>
       </section>
       <section className=" content-center px-100">
