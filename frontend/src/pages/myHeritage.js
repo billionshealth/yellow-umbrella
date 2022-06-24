3123f5ab964e379f653c3ae594e352ab9722efdf
MAbbasRaza <96629590+MAbbasRaza@users.noreply.github.com>
2022-06-24 15:54:35 +0500

    Update myHeritage.js

--- frontend/src/pages/myHeritage.js
+++ frontend/src/pages/myHeritage.js
@@ -1,15 +1,14 @@
 export default function MyHeritage() {
-
   const closeTab = () => window.close(``, `_parent`, ``);
 
   return (
-    <div className="flex flex-col justify-center csm:p-0 csm:justify-start bg-pink-1000 cmd:w-full cxl:text-lg px-24 cxl:px-64">
-      <section className="top my-10 ">
-        <h1 className="text-4xl cxl:text-5xl csm:text-2xl font-extrabold text-gray-800 m-10 px-2 csm:m-0">
+    <div className="flex flex-col justify-center csm:p-0 csm:justify-start bg-cbg cmd:w-full cxl:text-lg px-24 cxl:px-64 text-ctext">
+      <section className="top my-10 shadow-bshadow rounded-3xl">
+        <h1 className="text-4xl cxl:text-5xl csm:text-2xl font-extrabold text-ctitle m-10 px-2 csm:m-0">
           How to download data from MyHeritage
         </h1>
       </section>
-      <section className="mid flex flex-col justify-center px-100">
+      <section className="mid flex flex-col justify-center px-100 shadow-bshadow rounded-3xl mb-10">
         <div className="flex flex-col justify-center m-auto">
           <div className="m-5">
             <div className="flex 2xl:ml-36  cxl:ml-20 ">
@@ -23,7 +22,7 @@ export default function MyHeritage() {
               </p>
             </div>
             <img
-              className="mx-auto my-5 cmd:w-11/12 csm:w-full clg:w-7/12"
+              className="mx-auto my-5 cmd:w-11/12 csm:w-full clg:w-7/12 overflow-hidden border-4 border-solid border-bcolor rounded-2xl shadow-cshadow"
               src={process.env.PUBLIC_URL + "images/myheritage/step1.png"}
               alt="Sign In"
             />
@@ -38,7 +37,7 @@ export default function MyHeritage() {
               </p>
             </div>
             <img
-              className="mx-auto my-5 cmd:w-11/12 csm:w-full clg:w-7/12"
+              className="mx-auto my-5 cmd:w-11/12 csm:w-full clg:w-7/12 overflow-hidden border-4 border-solid border-bcolor rounded-2xl shadow-cshadow"
               src={process.env.PUBLIC_URL + "images/myheritage/step2.png"}
               alt="Raw Data"
             />
@@ -55,7 +54,7 @@ export default function MyHeritage() {
               </p>
             </div>
             <img
-              className="mx-auto my-5 cmd:w-11/12 csm:w-full clg:w-7/12"
+              className="mx-auto my-5 cmd:w-11/12 csm:w-full clg:w-7/12 overflow-hidden border-4 border-solid border-bcolor rounded-2xl shadow-cshadow"
               src={process.env.PUBLIC_URL + "images/myheritage/step3.png"}
               alt="Submit Raw Data Request"
             />
@@ -84,13 +83,19 @@ export default function MyHeritage() {
           <div className="m-5">
             <div className="flex 2xl:ml-36  cxl:ml-20">
               <p className="inline px-3 text-sky-700">6</p>
-              <p className="inline">Click below to return to the Yellow Umbrella home page and upload your file.</p>
+              <p className="inline">
+                Click below to return to the Yellow Umbrella home page and
+                upload your file.
+              </p>
             </div>
           </div>
           <div className="flex flex-col  mx-auto cxl:w-5/12">
             <button
               onClick={closeTab}
-              className=" rounded-full m-5 csm:text-xs csm:p-2 cmd:p-2 cmd:text-md  cxl:text-lg cxl:p-3 px-6 text-white bg-orange-400 baseline transition delay-150 ease-in-out hover:-translate-y-1 hover:scale-110 duration-300  border-2"
+              className=" rounded-full m-5 csm:text-xs csm:p-2 cmd:p-2 cmd:text-md  cxl:text-lg 
+              cxl:p-3 px-6 text-white bg-orange-400 baseline transition delay-150 ease-in-out 
+              hover:-translate-y-1 hover:scale-110 duration-300  border-2 shadow-cbshadow 
+              bg-gradient-to-r from-lgrad to-rgrad"
             >
               CLOSE TAB - READY TO UPLOAD
             </button>
