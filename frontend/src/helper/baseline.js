d2ac09dba4e9ba79dd8cad01550a7f988916b856
Chris Lovejoy <christopher.lovejoy1@gmail.com>
2022-06-24 12:36:57 +0100

    tweak placeholder image with comments

--- frontend/src/helper/baseline.js
+++ frontend/src/helper/baseline.js
@@ -180,8 +180,9 @@ export default function baseline(p){
         p.noiseSeed(noiseSeedVal);
         canvas = p.createCanvas(500, 500);
         canvas.id("nft-image")
-        canvas.parent("canvas")
+        canvas.parent("sketch-holder")
 
+        
         // canvas.style('display', 'centre');
 
 
